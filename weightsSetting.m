7b2e836ba96c1b004e185dfd9d945e39fff2a59c
batlau <31667232+batlau@users.noreply.github.com>
2017-10-10 12:56:40 +0300

    Update weightsSetting.m

--- weightsSetting.m
+++ weightsSetting.m
@@ -4,15 +4,15 @@ function [weights] = weightsSetting( imPatches, Dists, pyr ,dbPatchesStd )
 % Next, give a weight for each candidate based on its distance from the input patch.
 % denote m,n such that [m,n]=size(pyr{4})
 % Arguments:
-% imPatches ? (m ? 4) ª (n ? 4) ª 5 ª 5 matrix with the patches that were sampled from the 
+% imPatches ? (m ? 4) Âª (n ? 4) Âª 5 Âª 5 matrix with the patches that were sampled from the 
 % input image (pyr{4})
-% Dists ? (m ? 4) ª (n ? 4) ª 3 matrix with the distances returned from findNearestNeighbors.
-% pyr ? 7 ª 1 cell created using createPyramid
-% dbPatchesStd ? (m ? 4) ª (n ? 4) ª 3 matrix with the STDs of the neighbors patches returned
+% Dists ? (m ? 4) Âª (n ? 4) Âª 3 matrix with the distances returned from findNearestNeighbors.
+% pyr ? 7 Âª 1 cell created using createPyramid
+% dbPatchesStd ? (m ? 4) Âª (n ? 4) Âª 3 matrix with the STDs of the neighbors patches returned
 %from findNearestNeighbors.
 %
 % Outputs:
-% weights ? (m ? 4) ª (n ? 4) ª 3 matrix with the weights for each DB candidates
+% weights ? (m ? 4) Âª (n ? 4) Âª 3 matrix with the weights for each DB candidates
 %
 %calculating threshold - 
 [transX, transY] = translateImageHalfPixel(pyr{4});
@@ -24,19 +24,15 @@ sampleY = reshape(sampleY, [], 25);
 imPatchesReshaped = reshape(imPatches, [], 25);
 sampleX = imPatchesReshaped- sampleX;
 sampleY = imPatchesReshaped - sampleY;
-% sampleX = sampleX.^2;
-% sampleY = sampleY.^2;
+
 sampleX = sqrt(sum(sampleX.^2,2));
 sampleY = sqrt(sum(sampleY.^2,2));
 %calculating threshold
 thresh = (sampleX+sampleY)/2;
 thresh = reshape(thresh, size(imPatches,1), size(imPatches,2));
-% thresh = reshape(thresh, size(
 
 weights = zeros(size(Dists));
 chezkot = Dists(:,:,1:2).^2;
 weights(:,:,1:2) = exp(-chezkot./dbPatchesStd(:,:,1:2));
-%weights(:,:,3) = exp(-(weights.^2)./dbPatchesStd(:,:,3));
-% weights(passedThresh, 3) = myExp(passedThresh,3).^(-weights(passedThresh,3)/dbPatchesStd);
 weights(:,:,3) = exp(-(weights(:,:,3).^2)./dbPatchesStd(:,:,3));
 weights(Dists(:,:,3) > thresh) = 0;
